283ceab61f0ca217aa821e7119c978c0c43a847b
lhm8446 <lhm8446@gmail.com>
2016-11-30 15:35:13 +0900

    Initial commit

---
+++ app/src/main/java/com/bit2016/myapp/ui/tabs/Tab3Fragment.java
@@ -0,0 +1,23 @@
+package com.bit2016.myapp.ui.tabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bit2016.myapp.R;
+
+/**
+ * Created by bit-user on 2016-11-30.
+ */
+
+public class Tab3Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_tab3,container,false);
+    }
+}
